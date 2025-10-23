e0f098173f56b9901d7f5c04d9e7cbd627873eea
lishangbu <shangshili@hotmail.com>
2025-10-24 07:29:26 +0800

    refactor: 移除不必要的 MapperScan 注解，简化应用启动配置

--- orange-application/orange-admin-server/src/main/java/io/github/lishangbu/orange/admin/OrangeAdminApplication.java
+++ orange-application/orange-admin-server/src/main/java/io/github/lishangbu/orange/admin/OrangeAdminApplication.java
@@ -1,6 +1,5 @@
 package io.github.lishangbu.orange.admin;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,7 +9,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author lishangbu
  * @since 2025/10/11
  */
-@MapperScan(basePackages = "io.github.lishangbu.orange.**.mapper")
 @SpringBootApplication(scanBasePackages = "io.github.lishangbu.orange")
 public class OrangeAdminApplication {
   public static void main(String[] args) {
